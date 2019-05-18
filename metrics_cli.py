ddda2a1773fc6ee0474393d56cd0f94ecf80da42
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2019-05-18 15:36:20 +0100

    metrics_cli: prepare to read from a file or stdin

--- metrics_cli.py
+++ metrics_cli.py
@@ -2,12 +2,21 @@
 
 import argparse
 import os
+import sys
 
 def valid_input_file(f):
+    """Checks if a given file path leads to a valid file or stdin.
+    In case a file path is passed, will check if it is a valid file and is readable.
+    In case '-' is passed, will use stdin.
+    """
+
     if not (os.path.isfile(f) and os.access(f, os.R_OK)):  # a valid input file is a file that exists and is readable
         raise argparse.ArgumentTypeError("Cannot access this file! Are you sure it exists and you have read permission?")
     else:
-        return f
+        if f == '-':
+            return sys.stdin
+        else:
+            return open(f)
 
 def do_main():
     parser = argparse.ArgumentParser(description='Unbabel translation metrics stream parsing CLI.')
@@ -17,6 +26,11 @@ def do_main():
                         help='the window size (in minutes) for the translations to average.')
 
     args = parser.parse_args()
+
+
+    # If we have a file opened that is not stdin, we should close it
+    if args.input_file is not sys.stdin:
+        args.input_file.close()
 
 if __name__ == "__main__":
     do_main()
